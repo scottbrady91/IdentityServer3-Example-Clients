b9a563221a5a1789bc678466e7e6167efd85bf7f
Scott Brady <scottbrady91@outlook.com>
2015-04-30 18:52:34 +0100

    Initial OWIN client creation

---
+++ src/ScottBrady91.IdentityServer3.Example.Client.OWIN/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ScottBrady91.IdentityServer3.Example.Client.OWIN
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/ScottBrady91.IdentityServer3.Example.Client.OWIN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScottBrady91.IdentityServer3.Example.Client.OWIN.MvcApplication" Language="C#" %>
