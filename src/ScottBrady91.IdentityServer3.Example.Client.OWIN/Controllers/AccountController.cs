080a3683354946af0fbfe5ad44078318139aa4a7
Scott Brady <scottbrady91@outlook.com>
2015-05-02 10:00:20 +0100

    Updated OWIN client to use similar view structure to form post client

---
+++ src/ScottBrady91.IdentityServer3.Example.Client.OWIN/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ScottBrady91.IdentityServer3.Example.Client.OWIN.Controllers
+{
+    public sealed class AccountController : Controller
+    {
+        [Authorize]
+        public ActionResult SignIn()
+        {
+            return this.Redirect("/");
+        }
+
+        public ActionResult SignOut()
+        {
+            this.Request.GetOwinContext().Authentication.SignOut();
+            return this.Redirect("/");
+        }
+    }
+}
--- src/ScottBrady91.IdentityServer3.Example.Client.OWIN/Controllers/HomeController.cs
+++ src/ScottBrady91.IdentityServer3.Example.Client.OWIN/Controllers/HomeController.cs
@@ -1,26 +1,12 @@
-﻿using System.Security.Claims;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ScottBrady91.IdentityServer3.Example.Client.OWIN.Controllers
 {
-    public class HomeController : Controller
+    public sealed class HomeController : Controller
     {
         public ActionResult Index()
         {
             return this.View();
-        }
-
-        [Authorize]
-        public ActionResult Claims()
-        {
-            return this.View((this.User as ClaimsPrincipal).Claims);
-        }
-
-        public ActionResult Signout()
-        {
-            this.Request.GetOwinContext().Authentication.SignOut();
-            return this.Redirect("/Home/Claims");
         }
     }
 }
